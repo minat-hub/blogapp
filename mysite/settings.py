45ed73cef42270c50035d06794a8ac73b9b4fdc0
mina <mina3504ever@gmail.com>
2020-10-24 06:30:06 -0700

    change

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wx7i41znzyfb$)3%@z=ov-ezgo7!q#24#x5534po%aso6bu50a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
